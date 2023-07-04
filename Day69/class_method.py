3ebde2b548a4ee49787bee4cae9d14e3cabd7e45
Smit Padshala <112695070+Smitpadshala99@users.noreply.github.com>
2023-07-04 11:29:31 +0530

    Add files via upload

---
+++ Day69/class_method.py
@@ -0,0 +1,16 @@
+class Employee:
+    company = "Apple"
+    def show(self):
+        print(f"The name is {self.name} and company is {self.company}")
+
+    @classmethod
+    def changecompany(cls, newcompany):
+        cls.company = newcompany
+
+e1 = Employee()
+e1.name = "Smit"
+e1.show()
+e1.changecompany("Tesla")    
+e1.show()
+print(Employee.company)
+    
